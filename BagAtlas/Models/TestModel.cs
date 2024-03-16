91ea8359abc63bd417893966c59803606be751aa
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-16 22:15:08 +0100

    Serialization Test
    I have a problem with my own data utils

--- BagAtlas/Models/TestModel.cs
+++ BagAtlas/Models/TestModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BagAtlas.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,8 @@ using System.Threading.Tasks;
 
 namespace BagAtlas.Models {
     internal class TestModel {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public Vector3f Pos { get; set; }
     }
 }
--- BagAtlas/ViewModels/MainWindowViewModel.cs
+++ BagAtlas/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using BagAtlas.Models;
+using System;
+using System.Text.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
 
 namespace BagAtlas.ViewModels {
     internal class MainWindowViewModel {
+
+        private BindableCollection<TestModel> _tests = new BindableCollection<TestModel>();
+
+        public MainWindowViewModel() {
+            _tests.Add(new TestModel { Name = "One", Id = 1, Pos = new Utils.Vector3f(1.0f, 1.0f, 1.0f) });
+            _tests.Add(new TestModel { Name = "Two", Id = 2, Pos = new Utils.Vector3f(2.0f, 2.0f, 2.0f) });
+            _tests.Add(new TestModel { Name = "Three", Id = 3, Pos = new Utils.Vector3f(3.0f, 3.0f, 3.0f) });
+
+        }
+
+        public void Serialization() {
+            string jsonString = JsonSerializer.Serialize(_tests);    
+        }
     }
 }
