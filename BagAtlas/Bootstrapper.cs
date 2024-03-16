d08e8be20df8f5b15e3126c48a43aa75ac9b6885
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-16 18:58:49 +0100

    Init Caliburn Micro, "migrate" to the MVVM model, create a basic window

---
+++ BagAtlas/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using BagAtlas.ViewModels;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace BagAtlas {
+    internal class Bootstrapper : BootstrapperBase {
+        
+        public Bootstrapper() {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) {
+            DisplayRootViewForAsync<MainWindowViewModel>();
+        }
+
+    }
+}
