16d752af830cadebc466dc8b892ad1dc62160c72
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-19 21:25:58 +0100

    Serialization Test DONE

--- BagAtlas/Utils/Vector3f.cs
+++ BagAtlas/Utils/Vector3f.cs
@@ -6,35 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BagAtlas.Utils {
-    public class Vector3f : IJsonSerializable {
-        public float x;
-        public float y;
-        public float z;
-
-        // Constractors
-        public Vector3f() { }
-
-        public Vector3f(float value) { 
-            this.x = value;
-            this.y = value;
-            this.z = value;
-        }
-
-        public Vector3f(float x, float y, float z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public void Serialize(IJsonWriter writer) {
-            writer.WriteStartObject();
-            writer.WritePropertyName("x");
-            writer.WriteValue(X);
-            writer.WritePropertyName("y");
-            writer.WriteValue(Y);
-            writer.WritePropertyName("z");
-            writer.WriteValue(Z);
-            writer.WriteEndObject();
-        }
+    public class Vector3f { 
+        public float x { get; set; } 
+        public float y { get; set; } 
+        public float z { get; set; }
     }
 }
--- BagAtlas/ViewModels/MainWindowViewModel.cs
+++ BagAtlas/ViewModels/MainWindowViewModel.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
 using System.Net.NetworkInformation;
+using Newtonsoft.Json;
+using System.Windows.Media.Media3D;
+using BagAtlas.Utils;
 
 
 namespace BagAtlas.ViewModels {
@@ -16,15 +19,13 @@ namespace BagAtlas.ViewModels {
         private BindableCollection<TestModel> _tests = new BindableCollection<TestModel>();
 
         public MainWindowViewModel() {
-            _tests.Add(new TestModel { Name = "One", Id = 1, Pos = new Utils.Vector3f(1.0f, 1.0f, 1.0f) });
-            _tests.Add(new TestModel { Name = "Two", Id = 2, Pos = new Utils.Vector3f(2.0f, 2.0f, 2.0f) });
-            _tests.Add(new TestModel { Name = "Three", Id = 3, Pos = new Utils.Vector3f ( 3.0f, 3.0f, 3.0f )});
-
+            _tests.Add(new TestModel { Name = "One", Id = 1, Pos = new Vector3f{ x = 1.0f, y = 1.0f, z = 1.0f } });
+            _tests.Add(new TestModel { Name = "Two", Id = 2, Pos = new Vector3f { x = 1.0f, y = 1.0f, z = 1.0f } });
+            _tests.Add(new TestModel { Name = "Three", Id = 3, Pos = new Vector3f { x = 1.76876f, y = 1.0f, z = 1.0f } }); 
         }
 
         public void Serialization() {
-            string jsonString = JsonSerializer.Serialize(_tests);
-            Console.WriteLine(jsonString);
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(_tests);
         }
     }
 }
