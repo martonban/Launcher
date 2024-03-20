20ea1113d722ef5bc50f44af61fa1b84dd088213
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-20 13:15:52 +0100

    I/O Init

---
+++ BagAtlas/Utils/IO/FileManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// --------------------------------------------------------------------
+//                      FileManager - BagAtlas
+//                  Copyright Márton Bán (C) 2024
+//
+//  BagAtlas will handle multiple files. This class main responsibility
+//  is to track the files if we have a Bag Project
+// --------------------------------------------------------------------
+
+namespace BagAtlas.Utils.IO {
+    internal class FileManager {
+    }
+}
---
+++ BagAtlas/Utils/IO/FileWriter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagAtlas.Utils.IO {
+    internal class FileWiter {
+    }
+}
