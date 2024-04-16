424c414cb4417c591737174b4c649f978b4a9aa5
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-04-16 20:40:26 +0200

    LauncherBackend init

---
+++ LauncherBackend/AppStatusManagerSubsystem/AppStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LauncherBackend.AppStatusManagerSubsystem {
+    internal class AppStatus {
+        // General Data
+        private string _appRealitvePath { get; set; }
+
+        // User Data
+        private string _userDataPath { get; set; } 
+    }
+}
