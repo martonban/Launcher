38b11646752f12766e088632e91bb305fb108068
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-03 17:22:30 +0200

    Application Repository - Ready to test

--- LauncherBackend/Repository/AppDatabaseRepository.cs
+++ LauncherBackend/Repository/AppDatabaseRepository.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using LauncherBackend.Database;
+using LauncherBackend.Databases;
+using LauncherBackend.Exceptions;
+using LauncherBackend.Modells;
+using LauncherBackend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LauncherBackend.Repository {
-    internal class AppDatabaseRepository {
+    public class AppDatabaseRepository {
+        private ApplicationDataBase applicationDataBase = new ApplicationDataBase();
+        private bool doesApplicationDatabaseConnected = false;
+
+
+        //----------------------------------------------
+        //        GameData realated functions 
+        //----------------------------------------------
+        public void ConnectToApplicationDataBase(string databaseURL) {
+            try {
+                applicationDataBase.ConnectToApplicationDataBase(databaseURL);
+                this.doesApplicationDatabaseConnected = true;
+            } catch (GameDataBaseConnectionException exp) {
+                Console.WriteLine("Error: Cannot connect to the application database!");
+            }
+        }
+
+        public AppDTO GetApplicationByIDFromTheDatabase(int id) {
+            if (doesApplicationDatabaseConnected) {
+                try {
+                    return applicationDataBase.GetAppByID(id);
+                } catch (AppIsNotExistedInTheDatabaseException exp) {
+                    Console.WriteLine(exp.Message);
+                }
+            } else {
+                throw new ApplicationDataBaseConnectionException("Error: Application database in not connected, please connect to the database! Use: ApplicationController::ConnectToApplicationDataBase(string url) function!");
+            }
+            return new AppDTO();
+        }
+
+        public void InsertApp(AppDTO app) {
+            if (doesApplicationDatabaseConnected) {
+                try {
+                    applicationDataBase.InsertApplication(app);
+                } catch (AppIsExistsInTheDataBaseWithThisIDExeption exp) {
+                    Console.WriteLine(exp.Message);
+                }
+            } else {
+                throw new ApplicationDataBaseConnectionException("Error: Application database in not connected, please connect to the database! Use: ApplicationController::ConnectToApplicationDataBase(string url) function!");
+            }
+
+        }
+
+        public List<AppDTO> GetAllApps() {
+            if (doesApplicationDatabaseConnected) {
+                return applicationDataBase.GetAllApplications();
+            } else {
+                throw new ApplicationDataBaseConnectionException("Error: Application database in not connected, please connect to the database! Use: ApplicationController::ConnectToApplicationDataBase(string url) function!");
+            }
+        }
     }
 }
