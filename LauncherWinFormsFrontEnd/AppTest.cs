65263b971b0207434364ec35832913046d4f11a6
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-06 23:18:34 +0200

    App Test init

---
+++ LauncherWinFormsFrontEnd/AppTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LauncherWinFormsFrontEnd {
+    public partial class AppTest : Form {
+        public AppTest() {
+            InitializeComponent();
+        }
+
+        private void AppTest_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ LauncherWinFormsFrontEnd/Models/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LauncherWinFormsFrontEnd.Models {
+    public class App {
+        public int? Id { get; set; }
+        public string? AppName { get; set; }
+        public string? Description { get; set; }
+        public string? Suit { get; set; }
+        public string? Version { get; set; }
+        // Installation Data on the FTP File System
+        public string? FTPFolderPath { get; set; }
+        public string? FileName { get; set; }
+        // Media files on the FTP
+        public string? IconPath { get; set; }
+        public string? CoverPath { get; set; }
+
+        // FTP
+        public string? CurrentFTPPath { get; set; }
+    }
+}
