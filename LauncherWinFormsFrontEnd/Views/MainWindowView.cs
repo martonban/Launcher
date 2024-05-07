f06e118eea0e52715c2eaefce376fc256ddafb9b
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-07 18:40:13 +0200

    Base GUI init

---
+++ LauncherWinFormsFrontEnd/Views/MainWindowView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LauncherWinFormsFrontEnd.Views {
+    public partial class MainWindowView : Form {
+        public MainWindowView() {
+            InitializeComponent();
+        }
+    }
+}
